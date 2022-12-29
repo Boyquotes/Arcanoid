48386e8c7934a55983366bbf63986564472b1449
Sergey Nikitin <medex81@gmail.com>
2022-12-29 16:43:11 +0300

    Добавлены новые сервисы и тестовые сцены.

--- test.gd
+++ test.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Button
 
 var state_dict:Dictionary
 @onready var state:State = Services.state
@@ -6,14 +6,17 @@ var state_dict:Dictionary
 @onready var scenes:Scenes = Services.scenes
 @onready var sounds:Sounds = Services.sounds
 
+# Called when the node enters the scene tree for the first time.
 func _ready():
-	state_dict = state.get_dict("main")
-	logs.info("Main scene > ready")
-	
-func _exit_tree():
-	state.set_dict("main", {"state": 1})
-	logs.info("Main scene > exit")
+	state_dict = state.get_dict(name)
+	logs.info("Test scene > ready")
 
-func _on_button_pressed():
+
+func _exit_tree():
+	state.set_dict(name, {"state": 1})
+	logs.info("Test scene > exit")
+
+
+func _on_pressed():
 	sounds.play("res://addons/GameBackend/sounds/interface/click_001.ogg")
-	scenes.change_scene_from_path_with_transite("res://test.tscn")
+	scenes.change_scene_from_path_with_transite("res://main.tscn")
