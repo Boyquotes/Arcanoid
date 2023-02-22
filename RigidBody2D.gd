9cddff4f7a3ee800dd6e1a7ad941415bba444e84
Sergey Nikitin <medex81@gmail.com>
2023-02-23 00:05:57 +0300

    Изменена работа с интерактивами, добавлены новые блоки и функционал.

--- RigidBody2D.gd
+++ RigidBody2D.gd
@@ -1,21 +1,29 @@
 extends RigidBody2D
 
-@onready var _sounds:Sounds = Services.sounds
-@onready var _interactives:Interactives = Services.interactives
-@onready var _logs:LoggotLogger = Services.logs
+var _sounds:Sounds = Services.sounds
+var _logs:LoggotLogger = Services.logs
+var _interactive = null
+const _id_interactive = "BALL"
 
 
 func _ready():
 	assert(_logs)
+	assert(_sounds)
 	_logs.info("Body rect > " + name)
-	assert(_interactives)
-	_interactives.create(self, "BALL")
+	_interactive = Interactive.create(_id_interactive)
+	if _interactive:
+		_interactive.send_interactive_event.connect(_on_interactive_hitpoint_end)
+		add_child(_interactive)
 
 
 func _on_body_entered(body):
-	_interactives.interact(self, body)
+	if _interactive:
+		_interactive.interact(body)
 	match str(body.name):
 		"SB_Dynamic_perimetr":
 			_sounds.play("res://addons/GameBackend/sounds/impact/impactBell_heavy_004.ogg")
 		"CB2D_platform":
 			_sounds.play("res://addons/GameBackend/sounds/impact/impactGlass_light_000.ogg")
+			
+func _on_interactive_hitpoint_end(type, value):
+	queue_free()
--- game/items/SB_one_punch_rect.gd
+++ game/items/SB_one_punch_rect.gd
@@ -1,11 +1,18 @@
 extends StaticBody2D
 
-@onready var _logs:LoggotLogger = Services.logs
-@onready var _interactives:Interactives = Services.interactives
+var _logs:LoggotLogger = Services.logs
+var _interactive = null
+const _id_interactive = "BLUE_TILE"
 
 func _ready():
 	assert(_logs)
 	_logs.info("Body rect > " + name)
-	_interactives.create(self, "ONE_PUNCH_RECT")
+	_interactive = Interactive.create(_id_interactive)
+	if _interactive:
+		_interactive.send_interactive_event.connect(_on_interactive_hitpoint_end)
+		add_child(_interactive)
+		
+func _on_interactive_hitpoint_end(type, value):
+	queue_free()
 
 	
